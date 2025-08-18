a271e015998300478312d15d39b75675458413fe
NguyenVanGiang060202 <nguyenvangiang060202@gmail.com>
2025-08-18 23:56:18 +0700

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,14 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: true,
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 4173,
+    allowedHosts: ['*']
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
